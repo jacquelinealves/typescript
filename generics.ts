cbc0c7b07623733c729e4a9fde5b09226a2c070e
Jacqueline Alves <jacque.alvesilva@gmail.com>
2021-07-21 14:47:24 -0300

    feature: generics.

--- generics.ts
+++ generics.ts
@@ -1,3 +1,45 @@
 /**
  * GENERICS
+ *
+ * Permite que dentro de uma línguagem tipada, tenhamos algum tipo de flexibilidade
+ */
+
+/**
+ * Valores default
+ * S => State
+ * T => Type
+ * V => Value
+ * E => Element
+ */
+
+// () parenteses = atributos/parâmetros
+// <> tipos = genéricos
+
+type numOrStr = number | string;
+
+function useState<S extends numOrStr = string>() {
+  let state: S;
+
+  function getState() {
+    return state;
+  }
+
+  function setState(newState: S) {
+    state = newState;
+  }
+
+  return { getState, setState };
+}
+
+const newState = useState();
+
+newState.setState("foo"); // lê uma string
+console.log(newState.getState());
+
+newState.setState(false); // error por não atender aos tipos definidos
+console.log(newState.getState());
+
+/**
+ * No React usa-se bastante o generic da seguinte forma
+ * React.FC<Props, States>
  */
